b08cfc75ce00ce992411922074c27f472ce264f7
Sarwarul Rizvi <sarwarul.rizvi@gmail.com>
2022-03-24 17:19:33 +0100

    improved input validation. Iban allows only numbers and latin alphabets.

--- Assignment.BusinessLogic/Features/Iban/ValidateIban/ValidateIbanValidator.cs
+++ Assignment.BusinessLogic/Features/Iban/ValidateIban/ValidateIbanValidator.cs
@@ -1,12 +1,22 @@
 ﻿using FluentValidation;
+using System.Text.RegularExpressions;
 
 namespace Assignment.BusinessLogic.Features.Iban.ValidateIban
-{    
+{
     public sealed class ValidateIbanValidator : AbstractValidator<ValidateIbanRequest>
     {
         public ValidateIbanValidator()
         {
             RuleFor(r => r.Iban).NotEmpty().WithMessage("Iban is required");
-        }         
-    }    
+
+            RuleFor(r => r.Iban).Must(x => DoesNotContainSpecialCharacters(x)).WithMessage("Iban should not contain special characters");
+        }
+
+        private bool DoesNotContainSpecialCharacters(string iban)
+        {
+            var regexItem = new Regex("^[a-zA-Z0-9 ]*$");
+
+            return regexItem.IsMatch(iban);
+        }
+    }
 }
